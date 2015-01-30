36d9e9f877d74408890fb0c9461c67a87a85892a
lori-annw <loriannw123@gmail.com>
2015-01-29 19:14:12 -0500

    Update main.py

--- CS486 A2/main.py
+++ CS486 A2/main.py
@@ -4,18 +4,21 @@ with open('randTSP/10/instance_1.txt') as f:
     content = f.readlines()
     
 #we assume at lease one city
-dict = {}
+dictOfCities = {}
 numCities = int(content[0]);
 for x in range(1,numCities+1):
     contentlst = content[x].split(" ")
     city = contentlst[0]
     x = int(contentlst[1])
     y = int(contentlst[2])
-    dict[city] = (x,y)
+    dictOfCities[city] = (x,y)
 
-print (dict)
+print (dictOfCities)
 
 def g (city1, city2):
+    distance(city1,city2)
+
+def distance (city1,city2):
     x1 = city1[0]
     x2 = city2[0]
     y1 = city1[1]
@@ -23,8 +26,16 @@ def g (city1, city2):
     dist = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
     return dist
     
+    
+distances = {}
+for i in dictOfCities.keys():
+    for j in dictOfCities.keys():
+        
+        lst[j] = distance()
+    distances.append(lst)
+    
 
 #define loop here 
 
 
-print ( g (dict['A'], dict['D']) )
+print ( g (dictOfCities['A'], dictOfCities['D']) )
